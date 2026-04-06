49060d3dc05c151ac29403f0c6de51c6e7fb2380
Timothy Brits <timothybrits@icloud.com>
2026-04-06 08:24:44 +0200

    chore: use happy-dom as vitest environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'node',
+    environment: 'happy-dom',
     globals: true,
+    setupFiles: ['./src/test/setup.ts'],
   },
 })
